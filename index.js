54b8761a83cf51ea459dd3c7ba7cb62385b9d8ab
shoaib <shoaibhossain24@gmail.com>
2023-06-23 04:09:08 +0600

    add minor improvements

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 function rollFunction(){
-    document.getElementById("diceImg1").src = "images/dice" + Math.floor((Math.random()*6)+1)    +".png";
-    document.getElementById("diceImg2").src = "images/dice"+Math.floor((Math.random()*6)+1)  +".png";
+
+    
+    var diceImg1Value = Math.floor((Math.random()*6)+1);
+    var diceImg2Value = Math.floor((Math.random()*6)+1);
+    document.getElementById("diceImg1").src = "images/dice" +  diceImg1Value   +".png";
+    document.getElementById("diceImg2").src = "images/dice"+diceImg2Value +".png";
+
+if(diceImg1Value==diceImg2Value){
+    document.getElementById("winnerDeclaration").innerHTML = "Draw 🤝";
+}
+
+   else if(diceImg1Value > diceImg2Value){
+    document.getElementById("winnerDeclaration").innerHTML = "Player 1 Wins!! ✨";
+}
+else{  document.getElementById("winnerDeclaration").innerHTML = "Player 2 Wins!! ⚡";
+}
+
+
 
 }
